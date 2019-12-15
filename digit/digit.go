b108c9f447008790977b568ee33b36546bf48634
Jacky Tang <jackytck@gmail.com>
2019-12-15 13:06:24 +0800

    :sparkles: add JoinInts + JoinIntsBig

--- digit/digit.go
+++ digit/digit.go
@@ -2,6 +2,8 @@ package digit
 
 import (
 	"math/big"
+
+	"github.com/jackytck/gowboy/common"
 )
 
 // SliceInt returns the individual digits as a slice of int.
@@ -33,6 +35,34 @@ func SliceIntBig(n *big.Int) []int {
 		d = append([]int{int(m.Int64())}, d...)
 	}
 	return d
+}
+
+// JoinInts joins slice of single digit ints and return it as an int.
+func JoinInts(slice []int) int {
+	var sum int
+	len := len(slice) - 1
+	for i, v := range slice {
+		p := 1
+		for j := 0; j < len-i; j++ {
+			p *= 10
+		}
+		sum += p * v
+	}
+	return sum
+}
+
+// JoinIntsBig joins slice of single digit ints into a big.Int.
+func JoinIntsBig(slice []int) *big.Int {
+	sum := big.NewInt(0)
+	ten := big.NewInt(10)
+	p := common.Exp(10, len(slice)-1)
+	for _, v := range slice {
+		z := big.NewInt(1)
+		z.Mul(p, big.NewInt(int64(v)))
+		sum.Add(sum, z)
+		p.Div(p, ten)
+	}
+	return sum
 }
 
 // SumBig sums the digits of a big number.
--- digit/digit_test.go
+++ digit/digit_test.go
@@ -64,6 +64,61 @@ func ExampleSliceIntBig() {
 	// Output: [3 1 4 1 5 9 2 6 5 3 5 8 9 7 9 3 2 3 8]
 }
 
+func TestJoinInts(t *testing.T) {
+	type args struct {
+		slice []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1:", args{[]int{1, 2, 3, 4, 5, 6, 7}}, 1234567},
+		{"Case 2:", args{[]int{0, 2, 3, 0, 5, 0, 7}}, 230507},
+		{"Case 3:", args{[]int{2, 3, 5, 7, 1}}, 23571},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := digit.JoinInts(tt.args.slice); got != tt.want {
+				t.Errorf("JoinInts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleJoinInts() {
+	fmt.Println(digit.JoinInts([]int{2, 3, 5, 7, 1, 1, 1, 3, 1, 7}))
+	// Output: 2357111317
+}
+
+func TestJoinIntsBig(t *testing.T) {
+	type args struct {
+		slice []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *big.Int
+	}{
+		{"Case 1", args{[]int{1, 2, 3, 4, 5, 6, 7}}, big.NewInt(1234567)},
+		{"Case 2", args{[]int{0, 2, 3, 0, 5, 0, 7}}, big.NewInt(230507)},
+		{"Case 3", args{[]int{2, 3, 5, 7, 1}}, big.NewInt(23571)},
+		{"Case 4", args{[]int{2, 2, 4, 6, 3, 8, 7, 2, 3, 6, 9, 2, 8, 7, 8, 5, 7, 1}}, big.NewInt(224638723692878571)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := digit.JoinIntsBig(tt.args.slice); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("JoinIntsBig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleJoinIntsBig() {
+	fmt.Println(digit.JoinIntsBig([]int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8}))
+	// Output: 3141592653589793238
+}
+
 func TestSumBig(t *testing.T) {
 	type args struct {
 		n *big.Int
