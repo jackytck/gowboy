586c3681c6b16359e8a985c25769b95dd23b9175
Jacky Tang <jackytck@gmail.com>
2019-12-26 14:04:42 +0800

    :sparkles: add pentagon + hexagon + heptagonal + octagonal numbers

--- number/number.go
+++ number/number.go
@@ -24,3 +24,51 @@ func TriangleNumber(n int) int {
 func IsTriangleNumber(n int) bool {
 	return IsSquareNumber(8*n + 1)
 }
+
+// PentagonNumber returns the n-th pentagonal number.
+// e.g. 1, 5, 12, 22, 35, ...
+func PentagonNumber(n int) int {
+	return n * (3*n - 1) / 2
+}
+
+// IsPentagonNumber determines if a number is a pentagonal number.
+func IsPentagonNumber(n int) bool {
+	t := (math.Sqrt(24*float64(n)+1) + 1) / 6
+	return t == math.Floor(t)
+}
+
+// HexagonNumber returns the n-th hexagonal number.
+// e.g. 1, 6, 15, 28, 45, ...
+func HexagonNumber(n int) int {
+	return n * (2*n - 1)
+}
+
+// IsHexagonNumber determines if a number is a hexagonal number.
+func IsHexagonNumber(n int) bool {
+	t := (math.Sqrt(8*float64(n)+1) + 1) / 4
+	return t == math.Floor(t)
+}
+
+// HeptagonalNumber returns the n-th heptagonal number.
+// e.g. 1, 7, 18, 34, 55, ...
+func HeptagonalNumber(n int) int {
+	return n * (5*n - 3) / 2
+}
+
+// IsHeptagonalNumber determines if a number is a heptagonal number.
+func IsHeptagonalNumber(n int) bool {
+	t := (math.Sqrt(40*float64(n)+9) + 3) / 10
+	return t == math.Floor(t)
+}
+
+// OctagonalNumber returns the n-th octagonal number.
+// e.g. 1, 8, 21, 40, 65, ...
+func OctagonalNumber(n int) int {
+	return n * (3*n - 2)
+}
+
+// IsOctagonalNumber determines if a number is a octagonal number.
+func IsOctagonalNumber(n int) bool {
+	t := (math.Sqrt(3*float64(n)+1) + 1) / 3
+	return t == math.Floor(t)
+}
--- number/number_test.go
+++ number/number_test.go
@@ -138,3 +138,273 @@ func ExampleIsTriangleNumber() {
 	fmt.Println(number.IsTriangleNumber(78))
 	// Output: true
 }
+
+func TestPentagonNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1", args{0}, 0},
+		{"Case 2", args{1}, 1},
+		{"Case 3", args{2}, 5},
+		{"Case 4", args{3}, 12},
+		{"Case 5", args{4}, 22},
+		{"Case 6", args{5}, 35},
+		{"Case 7", args{6}, 51},
+		{"Case 8", args{7}, 70},
+		{"Case 9", args{8}, 92},
+		{"Case 10", args{9}, 117},
+		{"Case 11", args{10}, 145},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.PentagonNumber(tt.args.n); got != tt.want {
+				t.Errorf("PentagonNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExamplePentagonNumber() {
+	fmt.Println(number.PentagonNumber(15))
+	// Output: 330
+}
+
+func TestIsPentagonNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Case 1", args{0}, false},
+		{"Case 2", args{1}, true},
+		{"Case 3", args{5}, true},
+		{"Case 4", args{52}, false},
+		{"Case 5", args{71}, false},
+		{"Case 6", args{3290}, true},
+		{"Case 7", args{4187}, true},
+		{"Case 8", args{4032}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.IsPentagonNumber(tt.args.n); got != tt.want {
+				t.Errorf("IsPentagonNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleIsPentagonNumber() {
+	fmt.Println(number.IsPentagonNumber(330))
+	// Output: true
+}
+
+func TestHexagonNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1", args{0}, 0},
+		{"Case 2", args{1}, 1},
+		{"Case 3", args{2}, 6},
+		{"Case 4", args{3}, 15},
+		{"Case 5", args{4}, 28},
+		{"Case 6", args{5}, 45},
+		{"Case 7", args{6}, 66},
+		{"Case 8", args{7}, 91},
+		{"Case 9", args{8}, 120},
+		{"Case 10", args{9}, 153},
+		{"Case 11", args{10}, 190},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.HexagonNumber(tt.args.n); got != tt.want {
+				t.Errorf("HexagonNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleHexagonNumber() {
+	fmt.Println(number.HexagonNumber(17))
+	// Output: 561
+}
+
+func TestIsHexagonNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Case 1", args{0}, false},
+		{"Case 2", args{1}, true},
+		{"Case 3", args{28}, true},
+		{"Case 4", args{154}, false},
+		{"Case 5", args{189}, false},
+		{"Case 6", args{861}, true},
+		{"Case 7", args{946}, true},
+		{"Case 8", args{947}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.IsHexagonNumber(tt.args.n); got != tt.want {
+				t.Errorf("IsHexagonNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleIsHexagonNumber() {
+	fmt.Println(number.IsHexagonNumber(561))
+	// Output: true
+}
+
+func TestHeptagonalNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1", args{0}, 0},
+		{"Case 2", args{1}, 1},
+		{"Case 3", args{2}, 7},
+		{"Case 4", args{3}, 18},
+		{"Case 5", args{4}, 34},
+		{"Case 6", args{5}, 55},
+		{"Case 7", args{6}, 81},
+		{"Case 8", args{7}, 112},
+		{"Case 9", args{8}, 148},
+		{"Case 10", args{9}, 189},
+		{"Case 11", args{10}, 235},
+		{"Case 12", args{27}, 1782},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.HeptagonalNumber(tt.args.n); got != tt.want {
+				t.Errorf("HeptagonalNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleHeptagonalNumber() {
+	fmt.Println(number.HeptagonalNumber(19))
+	// Output: 874
+}
+
+func TestIsHeptagonalNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Case 1", args{0}, false},
+		{"Case 2", args{1}, true},
+		{"Case 3", args{55}, true},
+		{"Case 4", args{111}, false},
+		{"Case 5", args{149}, false},
+		{"Case 6", args{235}, true},
+		{"Case 7", args{1782}, true},
+		{"Case 8", args{1783}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.IsHeptagonalNumber(tt.args.n); got != tt.want {
+				t.Errorf("IsHeptagonalNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleIsHeptagonalNumber() {
+	fmt.Println(number.IsHeptagonalNumber(874))
+	// Output: true
+}
+
+func TestOctagonalNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Case 1", args{0}, 0},
+		{"Case 2", args{1}, 1},
+		{"Case 3", args{2}, 8},
+		{"Case 4", args{3}, 21},
+		{"Case 5", args{4}, 40},
+		{"Case 6", args{5}, 65},
+		{"Case 7", args{6}, 96},
+		{"Case 8", args{7}, 133},
+		{"Case 9", args{8}, 176},
+		{"Case 10", args{9}, 225},
+		{"Case 11", args{10}, 280},
+		{"Case 12", args{18}, 936},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.OctagonalNumber(tt.args.n); got != tt.want {
+				t.Errorf("OctagonalNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleOctagonalNumber() {
+	fmt.Println(number.OctagonalNumber(23))
+	// Output: 1541
+}
+
+func TestIsOctagonalNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Case 1", args{0}, false},
+		{"Case 2", args{1}, true},
+		{"Case 3", args{65}, true},
+		{"Case 4", args{67}, false},
+		{"Case 5", args{95}, false},
+		{"Case 6", args{176}, true},
+		{"Case 7", args{936}, true},
+		{"Case 8", args{939}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := number.IsOctagonalNumber(tt.args.n); got != tt.want {
+				t.Errorf("IsOctagonalNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleIsOctagonalNumber() {
+	fmt.Println(number.IsOctagonalNumber(1541))
+	// Output: true
+}
